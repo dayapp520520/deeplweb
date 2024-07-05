f485812cb03682e256721b6e6d8ad07b941b305e
dayapp520520 <130257106+dayapp520520@users.noreply.github.com>
2024-07-06 00:44:27 +0800

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -34,7 +34,7 @@ app.post('/translate', async (req, res) => {
 
   try {
     // Make a request to the translation API
-    const apiResponse = await fetch('https://pekmist-deeplx.hf.space/translate222222222', {
+    const apiResponse = await fetch('http://pro.gmail-mail.us/translate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
